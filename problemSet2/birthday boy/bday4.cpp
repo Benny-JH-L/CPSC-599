2760a15f39b0ad67f4fe713747345c3a04f32e4b
Benny-JH-L <133819720+Benny-JH-L@users.noreply.github.com>
2025-01-26 16:58:35 -0700

    need to calc. bobby's b-day now

--- problemSet2/birthday boy/bday4.cpp
+++ problemSet2/birthday boy/bday4.cpp
@@ -14,8 +14,12 @@ https://ucalgary.kattis.com/courses/CPSC_599-4/Winter_2025/assignments/b7bqkq/pr
 
 using namespace std;
 
+void gapTest();
+void nextBdayTest();
+
 static unordered_map<int, int> daysInMonthMap;
 static unordered_map<int, set<int>> bdayMap;
+static tuple<int, int> oct27(10, 27);
 
 int calcDaysBetweenMonths(int startMonth, int endMonth) // helper func for 'calcGap'
 {
@@ -63,6 +67,120 @@ int calcGap(tuple<int, int> startingDate, tuple<int, int> endDate)
     cout << startingM << "-" << startingD << " to " << endM << "-" << endD << " | Gap: " << gap << endl; 
 
     return gap;
+}
+
+tuple<int, int> getNextbday(tuple<int, int> currentbday)
+{
+    tuple<int, int> returnTup = currentbday;
+    auto [currM, currD] = currentbday;
+
+    bool nextBDayInAnotherMonth = true;
+    for (int day :  bdayMap[currM])
+    {
+        if (day > currD)
+        {
+            returnTup =  make_tuple(currM, day);
+            nextBDayInAnotherMonth = false;
+            break;
+        }
+    }
+
+    for (int monthKey = currM + 1, counter = 0; nextBDayInAnotherMonth && counter < bdayMap.size() ; monthKey = (monthKey + 1) % 13, counter++)
+    {
+        if (bdayMap[monthKey].size() > 0)  // return the month's first bday 
+        {
+            returnTup = make_tuple(monthKey, *bdayMap[monthKey].begin());   
+            break;
+        }
+    }
+
+    // debug
+    auto [m1, d1] = returnTup;
+    cout << "bday after " << currM << "-" << currD << " is " << m1 << "-" << d1 << endl;
+
+    return returnTup; // if there no/or 1 bday's inside the map, return the input.
+}
+
+int main()
+{
+    // initialize map
+    vector<int> monthsWith30Days = {4, 6, 9, 11};
+    vector<int> monthsWith31Days = {1, 3, 5, 7, 8, 10, 12};
+
+    for (int i : monthsWith30Days)
+        daysInMonthMap[i] = 30;
+    for (int i : monthsWith31Days)
+        daysInMonthMap[i] = 31;
+    daysInMonthMap[2] = 28;
+
+    // testing
+    // gapTest();
+    nextBdayTest();
+    // end of test
+
+    // Get employee birthdays and store into a hash map
+    int numEmployee = 101, counter = 0;
+    string line;
+    getline(cin, line);
+    istringstream iss(line);
+    iss >> numEmployee;
+
+    while(counter < numEmployee && getline(cin, line) && line != "")
+    {
+        iss = istringstream(line);
+        int month, day;
+        string employeeName;
+        char separator;
+        iss >> employeeName >> month >> separator >> day;
+        bdayMap[month].insert(day);
+        counter++;
+    }
+
+    // nextBdayTest(); // debug
+
+    tuple<int, int> firstbdayAfterOct27 = getNextbday(oct27);
+    auto [firstbdayAfterOct27M, firstbdayAfterOct27D] = firstbdayAfterOct27;
+    tuple<int, int> bdayToCompare = firstbdayAfterOct27;
+    tuple<int, int> longestRunningbday = firstbdayAfterOct27;
+    int longestGap = calcGap(oct27, firstbdayAfterOct27);
+
+    while(true)
+    {
+        tuple<int, int> nextbday = getNextbday(bdayToCompare);
+        auto [nextbdayM, nextbdayD] = nextbday;
+        // auto [m1, d1] = bdayToCompare;
+        
+        int gapTmp = calcGap(bdayToCompare, nextbday);
+
+        if (gapTmp > longestGap)
+        {
+            longestGap = gapTmp;
+            longestRunningbday = nextbday;
+        }
+        else if (gapTmp == longestGap)  // case, check which bday is the soonest
+        {
+            int tmp1 = calcGap(oct27, longestRunningbday);
+            int tmp2 = calcGap(oct27, nextbday);
+
+            if (tmp1 == 0)
+                longestRunningbday = nextbday;
+            else if (tmp2 != 0 && tmp2 < tmp1)
+            {
+                longestRunningbday = nextbday;
+            }
+        }
+
+        if (nextbdayM == firstbdayAfterOct27M && nextbdayD == firstbdayAfterOct27D) // if nextbday is the samebday as firstbdayAfterOct27, then exit loop
+            break;
+
+        bdayToCompare = nextbday;
+    }
+
+    auto [longestRunningMonth, longestRunningDay] = longestRunningbday;
+    cout << "\nThe longest running bday (makes longest gap) is: " << longestRunningMonth << '-' << longestRunningDay << endl;
+
+    // calc Bobby's fake b-day
+
 }
 
 void gapTest()
@@ -123,43 +241,35 @@ void gapTest()
     calcGap(oct28, oct28);
 }
 
-int main()
+void addTupleToBdayMap( tuple<int, int> tup)    // helper function
 {
-    // initialize map
-    vector<int> monthsWith30Days = {4, 6, 9, 11};
-    vector<int> monthsWith31Days = {1, 3, 5, 7, 8, 10, 12};
-
-    for (int i : monthsWith30Days)
-        daysInMonthMap[i] = 30;
-    for (int i : monthsWith31Days)
-        daysInMonthMap[i] = 31;
-    daysInMonthMap[2] = 28;
-
-    // testing
-
-    gapTest();
-    // end of test
-
-    // Get employee birthdays and store into a hash map
-    // int numEmployee = 101, counter = 0;
-    // string line;
-
-    // getline(cin, line);
-    // istringstream iss(line);
-    // iss >> numEmployee;
-
-    // while(counter < numEmployee && getline(cin, line) && line != "")
-    // {
-    //     iss = istringstream(line);
-    //     int month, day;
-    //     string employeeName;
-    //     char separator;
-    //     iss >> employeeName >> month >> separator >> day;
-    //     bdayMap[month].insert(day);
-    //     counter++;
-    // }
-
-    // bdayMap[10].insert(27); // add today's date (Oct. 27)
-
+    auto [key, day] = tup;
+    bdayMap[key].insert(day);
 }
 
+void nextBdayTest()
+{
+    bdayMap.clear();
+
+    // when there are 'no' bdays
+    tuple<int, int> oct27(10, 27); // NOTE: a b-day can also occur today (Oct 27)!
+    getNextbday(oct27);
+
+    // when there is 1 bday
+    cout << endl;
+    addTupleToBdayMap(oct27);
+    getNextbday(oct27);
+    bdayMap.clear();
+
+    cout << endl;
+    tuple<int, int> jan9(1, 9);
+    tuple<int, int> sept20(9, 20);
+    tuple<int, int> nov11(11, 11);
+    addTupleToBdayMap(jan9);
+    addTupleToBdayMap(sept20);
+    addTupleToBdayMap(nov11);
+    getNextbday(jan9);          // expected is 9-20
+    getNextbday(sept20);         // expected is 11-1
+    getNextbday(nov11);         // expected is 1-9
+    bdayMap.clear();
+}
