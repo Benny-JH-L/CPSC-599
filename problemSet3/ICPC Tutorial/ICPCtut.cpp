f05e6d5beaea4f93f9f7fab18337229f043f34fd
Benny-JH-L <bennyliang24@gmail.com>
2025-01-30 15:57:30 -0700

    REEEEE

--- problemSet3/ICPC Tutorial/ICPCtut.cpp
+++ problemSet3/ICPC Tutorial/ICPCtut.cpp
@@ -9,6 +9,11 @@ https://ucalgary.kattis.com/courses/CPSC_599-4/Winter_2025/assignments/ot4ggf/pr
 #include <sstream>
 #include <vector>
 
+#include <unordered_map>
+#include <tuple>
+#include <cmath>
+#include <algorithm>
+
 using namespace std;
 
 int main()
@@ -16,22 +21,40 @@ int main()
     string input;
     getline(cin, input);
     istringstream iss(input);
-    long long int m, n, t;
+    // unsigned long long int m, n, t;
+    int m, n;
+    int t;
     iss >> m >> n >> t;
 
-    long long int algorithmRunTime = 1;
+    // unsigned long long int algorithmRunTime = 1;
+    double algorithmRunTime = 1;
+
     vector<long long int> squares = {1, 2}; // 4, 8, 16
     vector<long long int> powers = {0, 1}; // 2, 3, 4
     bool calcTime = true;
 
     switch(t)
     {
-        case 1: // n!                           // NEED TO USE/MAKE OWN DATA STRUCTURE AS 10^9 * 10^9 is way to big
-            for (int i = n; i > 0; i--)
-                algorithmRunTime *= i;
+        case 1: // n!
+
+            if (n >= 14)    // 14! is larger then the largest m value.
+                cout << "TLE" << endl;
+            else 
+                cout << "AC" << endl;
+
+            // return 0;
+            // for (size_t i = n; i > 0; i--)
+            //     algorithmRunTime *= i;
 
         break;
         case 2: // 2^n, ex. 2^13 = 2^8 * 2^4 * 2^1 (using repeated squareing method)
+
+            if (n >= 30)
+            {
+                cout << "TLE" << endl;
+                return;
+            }  
+
             for (long long int power = 2; true; power = power * 2)
             {
                 if (power <= n)
@@ -41,7 +64,7 @@ int main()
                     squares.push_back(newSquareCalc);
                     powers.push_back(power);
 
-                    cout << "2^" << power << " = " << newSquareCalc << endl;    // debug
+                    // cout << "2^" << power << " = " << newSquareCalc << endl;    // debug
 
                     if (power == n)
                         break;
@@ -66,23 +89,23 @@ int main()
             }
 
         break;
-        case 3: // n^4
+        case 3: // n^4      // find the number 'n' such that n^4 >= 10^9 
             for (int i = 0; i <= 3; i++)
                 algorithmRunTime *= n;
             
         break;
-        case 4: // n^3
+        case 4: // n^3 // find the number 'n' such that n^3 >= 10^9 
             for (int i = 0; i <= 2; i++)
                 algorithmRunTime *= n;
         break;
-        case 5: // n^2
+        case 5: // n^2 // find the number 'n' such that n^2 >= 10^9 
             algorithmRunTime = n * n;
         break;
         case 6: // n*log2(n)
-
+            algorithmRunTime = n * log2(n);
         break;
         case 7: // n
-
+            algorithmRunTime = n;
         break;
     }
 
@@ -92,7 +115,7 @@ int main()
         cout << "TLE" << endl;
 
     // debug 
-    cout << "algo run time: " << algorithmRunTime << endl;
+    // cout << "algo run time: " << algorithmRunTime << endl;
 
     return 0;
 }
