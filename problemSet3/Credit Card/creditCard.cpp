f8320cd2c308c0eb570e265f2d584c3bc62bf477
Benny-JH-L <133819720+Benny-JH-L@users.noreply.github.com>
2025-02-01 18:23:08 -0700

    credit card finished.

--- problemSet3/Credit Card/creditCard.cpp
+++ problemSet3/Credit Card/creditCard.cpp
@@ -1,4 +1,9 @@
 
+/*
+https://open.kattis.com/problems/creditcard
+Passed all tests!
+*/
+
 #include <stdio.h>
 #include <iostream>
 #include <sstream>
@@ -15,22 +20,8 @@ double myRound(double& val, int decimalPlaces)
     return rounded;
 }
 
-// there is prolly some rounding error happening?
-// maybe submit again, because before i forgot to increment counter after each tes case
-
 int main()
 {
-    // double v1 = 5462.505;
-    // // printf("%3.f\n", v1); // dont work
-    // cout << "Before round: " << v1 << endl;
-    // roundThing(v1, 3);
-    // cout << "After round: " << v1 << endl;
-    // // printf("%3.f\n", v1); // dont work
-    // v1 = 3.14159;
-    // // printf("%3.f\n", roundThing(v1,3)); // dont work
-    // cout << roundThing(v1, 3) << endl;
-
-
     string input;
     getline(cin, input);
     istringstream iss(input);
@@ -47,7 +38,7 @@ int main()
         double interestAmount = outstandingBalance * (intererstRate / 100.f);  // convert from rate (%) to decimal
 
         // debug
-        // cout << intererstRate << endl;
+        // cout << "interest rate: " << intererstRate << "%" << endl;
         // cout << outstandingBalance << endl;
         // cout << monthlyPayment << endl;
 
@@ -57,10 +48,43 @@ int main()
         if (monthlyPayment > (outstandingBalance + myRound(interestAmount, 2)))    // not equal since interest will be applied to balance first
         {
             outputVector.push_back(1);   // if monthly payment > (outgoing balace + intrest) -> 1 payment needed
+            counter++;
             continue;
         }
         else
         {
+            for (int numberOfPayments = 0; numberOfPayments < 1200; numberOfPayments++)
+            {
+                interestAmount = outstandingBalance * (intererstRate / 100);  
+                interestAmount = round(interestAmount * 100) / 100; // need to round half cents up to the nearerst cent 
+                
+                outstandingBalance += interestAmount;   // add interst first
+                outstandingBalance = round(outstandingBalance * 100) / 100;
+                outstandingBalance -= monthlyPayment;   // then subtract monthly payment
+
+                if (outstandingBalance <= 0)
+                {
+                    outputVector.push_back(numberOfPayments + 1);   // add 1 since we start at 0
+                    break;
+                }
+                else if (numberOfPayments + 1 >= 1200)
+                {
+                    outputVector.push_back(-1);
+                    // cout << "number of payments before we find it as impossible: " << numberOfPayments + 1 << endl; // debug
+                    // cout << "remaining balance: " << outstandingBalance << endl;    //debug
+                    break;
+                }
+            }
+        }
+        counter++;
+    }
+
+    for (int result : outputVector)
+        cout << (result == -1 ? "impossible" : to_string(result)) << endl;
+
+    return 0;
+}
+
             // for (int numberOfPayments = 1; numberOfPayments < 1201; numberOfPayments++)
             // {
             //     interestAmount = outstandingBalance * (intererstRate / 100.f);  
@@ -85,37 +109,3 @@ int main()
             //         break;
             //     }
             // }
-            for (int numberOfPayments = 0; numberOfPayments < 1200; numberOfPayments++)
-            {
-                interestAmount = outstandingBalance * (intererstRate / 100.f);  
-
-                // cout << "before round interest rate: " <<  interestAmount << endl;
-                myRound(interestAmount, 2); // need to round half cents up to the nearerst cent
-                // cout << "after round interest rate: " <<  interestAmount << endl;
-                
-                outstandingBalance += interestAmount;   // add interst first
-                outstandingBalance -= monthlyPayment;   // then subtract monthly payment
-                // myRound(outstandingBalance, 2); // maybe?
-
-                if (outstandingBalance <= 0)
-                {
-                    outputVector.push_back(numberOfPayments + 1);   // add 1 since we start at 0
-                    break;
-                }
-                else if (numberOfPayments + 1 >= 1200)
-                {
-                    outputVector.push_back(-1);
-                    // cout << "number of payments before we find it as impossible: " << numberOfPayments + 1 << endl; // debug
-                    // cout << "remaining balance: " << outstandingBalance << endl;    //debug
-                    break;
-                }
-            }
-        }
-        counter++;
-    }
-
-    for (int result : outputVector)
-        cout << (result == -1 ? "impossible" : to_string(result)) << endl;
-
-    return 0;
-}
