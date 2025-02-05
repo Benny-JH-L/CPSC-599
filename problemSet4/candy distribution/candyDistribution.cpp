8b1110809f11fd1d8b397e21d26ef5b751f1e381
Benny-JH-L <133819720+Benny-JH-L@users.noreply.github.com>
2025-02-04 23:35:23 -0700

    q3 link added

---
+++ problemSet4/candy distribution/candyDistribution.cpp
@@ -0,0 +1,7 @@
+
+/*
+https://ucalgary.kattis.com/courses/CPSC_599-4/Winter_2025/assignments/j7jr2g/problems/candydistribution
+https://open.kattis.com/problems/candydistribution?tab=metadata 
+
+*/
+
