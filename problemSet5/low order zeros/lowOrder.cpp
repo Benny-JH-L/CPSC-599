622d4de13cb8b37ea172a7c0c9a6929ea7c8093f
Benny-JH-L <bennyliang24@gmail.com>
2025-02-13 20:12:33 -0700

    low order attempt

--- problemSet5/low order zeros/lowOrder.cpp
+++ problemSet5/low order zeros/lowOrder.cpp
@@ -1,21 +1,58 @@
 
+/*
+https://open.kattis.com/problems/loworderzeros?tab=metadata
+
+*/
+
 #include<stdio.h>
 #include<sstream>
 #include<iostream>
+#include<vector>
+#include<unordered_map>
 
 using namespace std;
 
-// size_t fact(size_t n)
-// {
-//     if (n == 1 || n == 0)
-//         return 1;
-    
-//     return n*fact(n-1);
-// }
-
 int main()
 {
-     
+    unordered_map<size_t, size_t> cache;    // key: an number to compute low order digit, Value: the low order digit
+
+    vector<string> outVec;
+    string input;
+    while(getline(cin, input) && input != "")
+    {
+        istringstream iss(input);
+        size_t num;
+        iss >> num;
+
+        if (num == 0)
+            break;
+        else if (cache.count(num))  // check if `num` has been calculate before
+        {
+            outVec.push_back(to_string(cache[num]));
+            continue;
+        }
+
+        // compute the low order digit for `num`
+        size_t resultSoFar = num;
+        for (size_t i = num - 1; i > 0; i--)
+        {
+            resultSoFar *= i;
+            size_t modResult = resultSoFar % 10;
+            if (modResult != 0)     // case 1: take the last digit (right most digit) 
+                resultSoFar = modResult;    // THIS IS NOT WORKING (RUN 25! EXAMPLE)
+            else                    // case 2: remove the trailing 0s
+                while (resultSoFar % 10 == 0)
+                    resultSoFar = (resultSoFar / 10);
+                resultSoFar = resultSoFar % 10; // do a mod here to remove any significant digits
+        }
+        cache[num] = resultSoFar;   // add into the cache
+        outVec.push_back(to_string(resultSoFar));   // add to output
+    }
+
+    // print output
+    for (string s : outVec)
+        cout << s << endl;
+    
     return 0;
 }
 
