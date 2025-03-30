e643809cf48ebb0653289cdb3e042880e7aff6a4
Benny-JH-L <133819720+Benny-JH-L@users.noreply.github.com>
2025-03-30 00:56:43 -0600

    fin problem set 10

--- problemSet10/A feast for cats/a_feast_for_cats.cpp
+++ problemSet10/A feast for cats/a_feast_for_cats.cpp
@@ -1,114 +1,128 @@
+
+/*
+https://ucalgary.kattis.com/courses/CPSC_599-4/Winter_2025/assignments/jkrmjr/problems/cats
+*/
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
+#include <set>
+#include <string>
 
 using namespace std;
 
-// Edge structure to represent distance between two cats
-struct Edge {
-    int u, v, weight;
-    
-    Edge(int _u, int _v, int _w) : u(_u), v(_v), weight(_w) {}
-    
-    bool operator<(const Edge& other) const {
-        return weight < other.weight;
-    }
-};
-
-// DisjointSet (Union-Find) data structure for Kruskal's algorithm
-class DisjointSet {
+class Graph 
+{
 private:
-    vector<int> parent, rank;
-    
+    int n;
+    vector<vector<int>> adj;
+
 public:
-    DisjointSet(int n) {
-        parent.resize(n);
-        rank.resize(n, 0);
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
+    Graph(int n, const vector<vector<int>>& adj) : n(n), adj(adj) {}
+
+    string mst(int m) 
+    {
+        return prims(m) ? "yes" : "no";
     }
-    
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
+
+private:
+    bool prims(int m) 
+    {
+        vector<int> best(n, 0);
+        set<int> remaining;
+        
+        for (int i = 1; i < n; i++) 
+            remaining.insert(i);
+        
+        vector<bool> visited(n, false);
+        visited[0] = true;
+        
+        // min heap of pairs (weight, vertex)
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        
+        int edges = 0;
+        int dist = 0;
+        
+        for (int i = 1; i < n; i++) 
+        {
+            heap.push({adj[0][i], i});
+            best[i] = adj[0][i];
         }
-        return parent[x];
-    }
-    
-    void unite(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
         
-        if (rootX == rootY) return;
-        
-        if (rank[rootX] < rank[rootY]) {
-            parent[rootX] = rootY;
-        } else {
-            parent[rootY] = rootX;
-            if (rank[rootX] == rank[rootY]) {
-                rank[rootX]++;
+        while (edges < n - 1) 
+        {
+            int w, v;
+            while (true) 
+            {
+                if (heap.empty()) 
+                    return false;   // in case the graph is not connected
+                
+                auto top = heap.top();
+                heap.pop();
+                w = top.first;
+                v = top.second;
+                if (!visited[v]) 
+                    break;
+            }
+            
+            edges++;
+            dist += w;
+            
+            if (dist + n > m) 
+                return false;
+            
+            visited[v] = true;
+            remaining.erase(v);
+            
+            for (auto w : remaining) 
+            {
+                if (adj[v][w] < best[w]) 
+                {
+                    best[w] = adj[v][w];
+                    heap.push({adj[v][w], w});
+                }
             }
         }
+        
+        return n + dist <= m;
     }
 };
 
-bool canServeMilk(int milk, int cats, const vector<Edge>& edges) {
-    // If there's only one cat (cat 0), we just need 1ml of milk
-    if (cats == 1) {
-        return milk >= 1;
+void read_adj(int c, vector<vector<int>>& adj) 
+{
+    int edges = (c * (c - 1)) / 2;
+    for (int i = 0; i < edges; ++i) 
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u][v] = w;
+        adj[v][u] = w;
     }
-    
-    // Create a graph with all cats as vertices
-    vector<Edge> allEdges = edges;
-    
-    // Sort edges by their weights (distances)
-    sort(allEdges.begin(), allEdges.end());
-    
-    // Apply Kruskal's algorithm to find the Minimum Spanning Tree
-    DisjointSet ds(cats);
-    int mstWeight = 0;
-    
-    for (const Edge& edge : allEdges) {
-        if (ds.find(edge.u) != ds.find(edge.v)) {
-            mstWeight += edge.weight;
-            ds.unite(edge.u, edge.v);
-        }
-    }
-    
-    // We need to traverse each edge in the MST twice (going and coming back)
-    // This causes us to spill 2 * MST weight amount of milk
-    int milkSpilled = 2 * mstWeight;
-    
-    // We need at least 1ml of milk for each cat
-    int milkForCats = cats;
-    
-    // Total milk required = milk spilled + milk needed for cats
-    return milk >= (milkSpilled + milkForCats);
 }
 
-int main() {
+void test_case(int m, int c) 
+{
+    vector<vector<int>> adj(c, vector<int>(c, 0));
+    read_adj(c, adj);
+    
+    if (m < c)
+        cout << "no" << endl;
+    else {
+        Graph g(c, adj);
+        cout << g.mst(m) << endl;
+    }
+}
+
+int main() 
+{
     int t;
     cin >> t;
     
-    while (t--) {
+    for (int i = 0; i < t; i++) 
+    {
         int m, c;
         cin >> m >> c;
-        
-        vector<Edge> edges;
-        
-        // Read distances between pairs of cats
-        for (int i = 0; i < c * (c - 1) / 2; i++) {
-            int u, v, dist;
-            cin >> u >> v >> dist;
-            edges.push_back(Edge(u, v, dist));
-        }
-        
-        if (canServeMilk(m, c, edges)) {
-            cout << "yes" << endl;
-        } else {
-            cout << "no" << endl;
-        }
+        test_case(m, c);
     }
     
     return 0;
--- problemSet10/red blue spanning tree/red_blue_spanning_tree.cpp
+++ problemSet10/red blue spanning tree/red_blue_spanning_tree.cpp
@@ -1,120 +1,76 @@
 
+/*
+https://ucalgary.kattis.com/courses/CPSC_599-4/Winter_2025/assignments/jkrmjr/problems/redbluetree
+*/
+
 #include <stdio.h>
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
-class DisjointSet 
+int find(vector<int>& d, int a) 
 {
-    private:
-        vector<int> parent, rank;
+    if (d[a] == -1) 
+        return a;
     
-    public:
-        DisjointSet(int n) 
-        {
-            parent.resize(n + 1);
-            rank.resize(n + 1, 0);
-            for (int i = 1; i <= n; i++) 
-                parent[i] = i;
-        }
-    
-        int find(int x) 
-        {
-            if (parent[x] != x) 
-                parent[x] = find(parent[x]);
-            return parent[x];
-        }
-    
-        bool unionSets(int x, int y) 
-        {
-            int px = find(x);
-            int py = find(y);
-            
-            if (px == py) return false;
-            
-            if (rank[px] < rank[py]) 
-                swap(px, py);
-            
-            parent[py] = px;
-            
-            if (rank[px] == rank[py]) 
-                rank[px]++;
-            
-            return true;
-        }
-    };
+    return d[a] = find(d, d[a]);
+}
 
-bool canFormSpanningTree(int n, vector<pair<char, pair<int, int>>>& edges, int k) 
+void join(vector<int>& d, int a, int b) 
 {
-    int m = edges.size();
+    a = find(d, a);
+    b = find(d, b);
+
+    if (a == b) 
+        return;
     
-    for (int blueMask = 0; blueMask < (1 << m); blueMask++) 
-    {
-        int blueCount = 0;
-        DisjointSet dsu(n);
-        
-        for (int i = 0; i < m; i++) 
-        {
-            if (blueMask & (1 << i)) 
-            {
-                if (edges[i].first != 'B') 
-                    continue;
-                
-                blueCount++;
-                if (!dsu.unionSets(edges[i].second.first, edges[i].second.second))
-                    blueCount--;
-            }
-        }
-        
-        // Second pass: Add red edges to complete spanning tree
-        for (int i = 0; i < m; i++) 
-        {
-            if (!(blueMask & (1 << i))) 
-            {
-                if (edges[i].first != 'R') continue;
-                
-                // if (!dsu.unionSets(edges[i].second.first, edges[i].second.second)) {
-                // }
-            }
-        }
-        
-        int components = 0;
-        for (int i = 1; i <= n; i++) 
-        {
-            if (dsu.find(i) == i) 
-                components++;
-        }
-        
-        if (components == 1 && blueCount == k) 
-            return true;
-    }
-    
-    return false;
+    d[a] = b;
 }
 
 int main() 
 {
     string input;
-    getline(cin, input);
     int n, m, k;
+    getline(cin, input);
     istringstream iss(input);
     iss >> n >> m >> k;
-    
-    vector<pair<char, pair<int, int>>> edges(m);
-    
-    for (int i = 0; i < m && getline(cin, input); i++) 
+
+    vector<int> d1(n, -1);
+    vector<int> d2(n, -1);
+
+    for(int i = 0; i < m && getline(cin, input); i++) 
     {
-        char color;
-        int from, to;
+        char c;
+        int n1, n2;
         iss = istringstream(input);
-        iss >> color >> from >> to;
-        edges[i] = {color, {from, to}};
+        iss >> c >> n1 >> n2;
+        n1--; n2--;
+
+        if(c == 'R') 
+            join(d1, n1, n2);
+        else 
+            join(d2,n1,n2);
     }
-    
-    cout << (canFormSpanningTree(n, edges, k) ? 1 : 0) << endl;
-    
-    return 0;
+
+    int blue_avail = 0;
+    for(int i = 0; i < n; i++) 
+    {
+        if(d2[i] != -1) 
+            blue_avail++;
+    }
+
+    int blue_need = 0;
+    for(int i = 0; i < n; i++) 
+    {
+        if(d1[i] == -1) 
+            blue_need++;
+    }
+    blue_need--;
+
+    if(blue_avail < k || blue_avail < blue_need || blue_need > k) 
+        cout << 0 << endl;
+    else 
+        cout << 1 << endl;
 }
